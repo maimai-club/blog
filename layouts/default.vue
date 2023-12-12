91ea8501dfedb39ac4bf7634bf8591a7c899be3b
rui-puipui <rui.o3i.1106@gmail.com>
2023-12-12 15:22:30 +0000

    owari

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
-        これはペンです
+        <v-app id="inspire">
+
+            <v-app-bar color="primary"/>
+            <v-navigation-drawer color="secondary"/>
+
+            <v-content class="bg-red blue">
+                <slot/>
+            </v-content>
+        </v-app>
     </div>
 </template>
