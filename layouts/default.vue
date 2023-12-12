463e39258f95fc161ef924d23a7f897082aa909e
rui-puipui <rui.o3i.1106@gmail.com>
2023-12-12 15:30:32 +0000

    あささささ

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <v-app id="inspire">
+
+            <v-app-bar color="primary"/>
+            <v-navigation-drawer color="secondary"/>
+
+            <v-content class="bg-red blue">
+                <slot/>
+            </v-content>
+        </v-app>
+    </div>
+</template>
