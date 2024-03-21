56f05ed1c85ab17e02c2e901cd7c5abf6eb3ff9e
eve68k <25098602+eve68k@users.noreply.github.com>
2024-03-21 12:39:51 +0000

    add author and tags

--- docs/.vitepress/components/ArticleList.vue
+++ docs/.vitepress/components/ArticleList.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ContentData } from 'vitepress'
+import { ContentData, withBase } from 'vitepress'
 import { data as articles } from '../articles.data.ts'
 const newArticles: ContentData[] = [...articles]// .reverse().slice(0, 6)
 console.log(articles)
@@ -8,10 +8,12 @@ console.log(newArticles)
 
 <template>
   <div>
-    記事一覧だよ☆
+    <h1>記事一覧だよ☆</h1>
     <ul>
       <li v-for="(article, idx) in newArticles" :key="idx">
-        <span v-if="article.frontmatter.title">{{ article.frontmatter.title }}</span>
+        <p v-if="article.frontmatter.title">タイトル：<a :href="withBase(article.url)">{{ article.frontmatter.title }}</a></p>
+        <p v-if="article.frontmatter.author">著者：{{ article.frontmatter.author }}</p>
+        <p>タグ：<span v-for="(tag, idx2) in article.frontmatter.tags" :key="idx2">{{ tag }}</span></p>
       </li>
     </ul>
   </div>
