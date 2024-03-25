4d94637b87c728278d3799e16ba54c0fdcba41a8
eve68k <25098602+eve68k@users.noreply.github.com>
2024-03-25 15:18:11 +0000

    update spacing

--- docs/.vitepress/components/ArticleCard.vue
+++ docs/.vitepress/components/ArticleCard.vue
@@ -11,20 +11,20 @@ const author: Author = getAuthorById(props.article.author)
 
 <template>
   <div class="border border-gray-300 rounded-lg my-4">
-    <div class="px-7">
-      <p class="flex items-center gap-x-3" v-if="author.name">
+    <div class="px-7 py-3">
+      <div class="my-2 flex items-center gap-x-3" v-if="author.name">
         <img class="rounded-full aspect-square max-h-9" v-if="author.icon" :src="author.icon" />
         <span class="text-xl">{{ author.name }}</span>
-      </p>
-      <p class="text-3xl flex items-center" v-if="props.article.title">
+      </div>
+      <div class="my-2 mb-4 text-3xl" v-if="props.article.title">
         <a :href="props.article.url">{{ props.article.title }}</a>
-      </p>
-      <p>
+      </div>
+      <div class="my-2">
         <span class="mr-2 border border-gray-300 rounded-lg px-2 py-1" v-for="(tag, idx2) in props.article.tags"
           :key="idx2">
           {{ tag }}
         </span>
-      </p>
+      </div>
     </div>
   </div>
 </template>
