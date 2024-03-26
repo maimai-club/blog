7c9f898eb471ee67db0c9fd095ae5fffc868b48b
eve68k <25098602+eve68k@users.noreply.github.com>
2024-03-26 13:16:09 +0000

    remove full-width

--- docs/.vitepress/components/ArticleCard.vue
+++ docs/.vitepress/components/ArticleCard.vue
@@ -12,12 +12,12 @@ const author: Author = getAuthorById(props.article.author)
 <template>
   <div class="border border-gray-300 rounded-lg my-4">
     <div class="px-7 py-3">
-      <div class="my-2 flex items-center gap-x-3" v-if="author.name">
+      <div class="my-1 flex items-center gap-x-3" v-if="author.name">
         <img class="rounded-full aspect-square max-h-9" v-if="author.icon" :src="author.icon" />
         <span class="text-xl">{{ author.name }}</span>
       </div>
-      <div class="my-2 mb-4 text-3xl w-full h-full block" v-if="props.article.title">
-        <a :href="props.article.url" class="w-full h-full block">{{ props.article.title }}</a>
+      <div class="my-2 mb-4 text-3xl" v-if="props.article.title">
+        <a :href="props.article.url">{{ props.article.title }}</a>
       </div>
       <div class="my-2">
         <span class="mr-2 border border-gray-300 rounded-lg px-2 py-1" v-for="(tag, idx2) in props.article.tags"
